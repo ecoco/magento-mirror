720b7c9ae22ccebad47bd0eed7f3da4553e930f9
Justus Krapp <jk@ecocode.de>
2018-02-19 23:14:08 +0100

    FIX: DB does not support the InnoDB storage engine

--- app/code/core/Mage/Install/Model/Installer/Db/Mysql4.php
+++ app/code/core/Mage/Install/Model/Installer/Db/Mysql4.php
@@ -58,8 +58,7 @@ class Mage_Install_Model_Installer_Db_Mysql4 extends Mage_Install_Model_Installe
      */
     public function supportEngine()
     {
-        $variables  = $this->_getConnection()
-            ->fetchPairs('SHOW VARIABLES');
-        return (!isset($variables['have_innodb']) || $variables['have_innodb'] != 'YES') ? false : true;
+        $variables = $this->_getConnection()->fetchPairs('SHOW ENGINES');
+        return (isset($variables['InnoDB']) && $variables['InnoDB'] != 'NO');
     }
 }
